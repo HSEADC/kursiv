af901df9e100f486c078ff5ed30ebbd0b569385e
ZakharDay <zakhar.day@gmail.com>
2022-12-20 18:47:10 +0300

    Build

---
+++ docs/6d6ea6c434a3ef966d23.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+  $('.burger').click(function () {
+    $('.burger').toggleClass('high');
+    $('.hidden_navigation').toggleClass('shown');
+    console.log('help');
+    $('.wrap').toggleClass('bg_blur');
+  });
+  $(function () {
+    $('.splash_menu a').each(function () {
+      $(this).on('click', function (e) {
+        e.preventDefault();
+        var scrollTo = $(this).attr('href');
+        var whitespace = scrollTo == '#about_us' ? 1 : 30;
+        $('html,body').animate({
+          scrollTop: $(scrollTo).offset().top - whitespace
+        }, 1000);
+      });
+    });
+  });
+});
