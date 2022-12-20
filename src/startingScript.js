c9828ff6c9a3411a478b66de2485de450e95d8c1
Matvey Vasilyev <hounsou734@gmail.com>
2022-12-20 18:33:20 +0300

    unfinished

--- src/startingScript.js
+++ src/startingScript.js
@@ -5,4 +5,19 @@ $(document).ready(function () {
     console.log('help')
     $('.wrap').toggleClass('bg_blur')
   })
+  $(function () {
+    $('.splash_menu a').each(function () {
+      $(this).on('click', function (e) {
+        e.preventDefault()
+        var scrollTo = $(this).attr('href')
+        var whitespace = scrollTo == '#about_us' ? 1 : 30
+        $('html,body').animate(
+          {
+            scrollTop: $(scrollTo).offset().top - whitespace
+          },
+          1000
+        )
+      })
+    })
+  })
 })
--- webpack.common.js
+++ webpack.common.js
@@ -12,7 +12,7 @@ module.exports = {
   },
   output: {
     filename: '[name].js',
-    path: path.resolve(__dirname, 'docs'),
+    path: path.resolve(__dirname, 'docs')
     clean: true
   },
   module: {
