0f7d523aacdf3a1b0b7a9d554a4bada7b7dfd07f
Matvey Vasilyev <hounsou734@gmail.com>
2022-12-24 07:55:33 +0300

    build

---
+++ docs/d61983aeafdeec5151ee.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+  $('.sub a').each(function () {
+    $(this).on('click', function (e) {
+      e.preventDefault();
+      console.log('hi');
+      var scrollTo = $(this).attr('href');
+      var whitespace = scrollTo == '#logo' ? 100 : 1;
+      $('html,body').animate({
+        scrollTop: $(scrollTo).offset().top - whitespace
+      }, 1000);
+    });
+  });
+  var sectionHeight = $('section').height();
+  var sectionHeightDouble = sectionHeight * 2 - 500;
+  var sectionHeightTriple = sectionHeight * 3 - 500;
+  var sectionHeightQuadriple = sectionHeight * 4 - 500;
+  var lis = document.getElementById('navbar').getElementsByClassName('sub');
+  console.log(lis[4]);
+  window.addEventListener('scroll', function (event) {
+    console.log(sectionHeight);
+    console.log(sectionHeightDouble);
+    console.log(sectionHeightTriple);
+    var element = document.getElementsByClassName('main');
+    var result = element[0].getBoundingClientRect().top * -1; // const rect = element.getBoundingClientRect()
+
+    console.log(result);
+
+    if (sectionHeight - 100 <= result && result <= sectionHeightDouble - 100) {
+      $(lis[0]).removeClass('active');
+      $(lis[1]).addClass('active');
+      $(lis[2]).removeClass('active');
+    } else if (sectionHeightDouble - 100 <= result && result <= sectionHeightTriple - 100) {
+      $(lis[0]).removeClass('active');
+      $(lis[1]).removeClass('active');
+      $(lis[2]).addClass('active');
+      $(lis[3]).removeClass('active');
+    } else if (result <= sectionHeight) {
+      $(lis[0]).addClass('active');
+      $(lis[1]).removeClass('active');
+    } else if (result >= sectionHeightTriple - 100 && result <= sectionHeightQuadriple - 100) {
+      $(lis[0]).removeClass('active');
+      $(lis[2]).removeClass('active');
+      $(lis[3]).addClass('active');
+      $(lis[4]).removeClass('active');
+    } else if (result >= sectionHeightQuadriple - 100) {
+      $(lis[0]).removeClass('active');
+      $(lis[3]).removeClass('active');
+      $(lis[4]).addClass('active');
+    }
+  });
+});
