21a0c64edfa94709e94fa0dbb82dc7126f50c96a
Matvey Vasilyev <hounsou734@gmail.com>
2023-05-16 15:30:45 +0300

    build

---
+++ docs/test.js
@@ -0,0 +1,25 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+
+;// CONCATENATED MODULE: ./src/jsbacics/store.js
+var store_text = '';
+
+function getText() {
+  return store_text;
+}
+
+function setText(newText) {
+  store_text = newText;
+}
+
+
+;// CONCATENATED MODULE: ./src/jsbasics.js
+
+
+console.log('test');
+console.log('getText', getText());
+setText('Yo');
+console.log('getText', getText());
+/******/ })()
+;
