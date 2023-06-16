5316be75facb518a1b56755b6f2c4f6b13b8c8e9
Matvey Vasilyev <hounsou734@gmail.com>
2023-06-16 21:47:26 +0300

    push

--- src/components/O_Header/O_Header.jsx
+++ src/components/O_Header/O_Header.jsx
@@ -1,16 +1,14 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import './O_Header.scss'
 import C_HeaderLinks from '../С_HeaderLinks/С_HeaderLinks.jsx'
 import C_LogoAndDescription from '../C_LogoAndDescription/C_LogoAndDescription.jsx'
 import C_HeaderTitleAndButton from '../C_HeaderTitleAndButton/C_HeaderTitleAndButton.jsx'
-export default class O_Header extends Component {
-  render() {
-    return (
-      <div className="O_Header">
-        <C_LogoAndDescription />
-        <C_HeaderLinks />
-        <C_HeaderTitleAndButton />
-      </div>
-    )
-  }
+export default function O_Header() {
+  return (
+    <div className="O_Header">
+      <C_LogoAndDescription />
+      <C_HeaderLinks />
+      <C_HeaderTitleAndButton />
+    </div>
+  )
 }
